65776c4e9675305ecc1f2afa546fa8dce43b9356
ekaterina_pchelenko <e.pchelenko@gmail.com>
2021-09-28 23:09:42 +0500

    worked with login.html and register.html

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login(request):
+    context = {
+        'title': 'Geekshop - Авторизация',
+    }
+    return render(request, 'users/login.html', context)
+
+
+def register(request):
+    context = {
+        'title': 'Geekshop - Регистрция',
+    }
+    return render(request, 'users/register.html', context)
