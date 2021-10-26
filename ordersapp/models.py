8f76f2739c39d8a4dfbf916f1d9226664aad507c
ekaterina_pchelenko <e.pchelenko@gmail.com>
2021-10-26 23:43:57 +0500

    finnished ordersapp (with error)

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -27,7 +27,7 @@ class Order(models.Model):
     created = models.DateTimeField(verbose_name='создан', auto_now_add=True)
     updated = models.DateTimeField(verbose_name='обновлён', auto_now=True)
     status = models.CharField(choices='', verbose_name=ORDER_STATUS_CHOICES, max_length=3, default='')
-
+    is_active = models.BooleanField(verbose_name='активный', default=True)
 
     def __str__(self):
         return f'Текущий заказ {self.pk}'
@@ -43,6 +43,14 @@ class Order(models.Model):
 
     def get_items(self):
         pass
+
+    def delete(self, using=None, keep_parents=False):
+
+        for item in self.orderitems.select_related():
+            item.product.quantity += item.quantity
+            item.save()
+        self.is_active = False
+        self.save()
 
 
 class OrderItem(models.Model):
--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -24,10 +24,10 @@ app_name = 'orders'
 urlpatterns = [
     path('', OrderList.as_view(), name='list'),
     path('create/', OrderCreate.as_view(), name='create'),
-    path('update/<int:pk>', OrderUpdate.as_view(), name='create'),
-    path('read/<int:pk>', OrderDetail.as_view(), name='create'),
-    path('delete/<int:pk>/', OrderDelete.as_view(), name='create'),
-    path('delete/<int:pk>/', order_forming_complete, name='forming_complete'),
+    path('update/<int:pk>', OrderUpdate.as_view(), name='update'),
+    path('read/<int:pk>', OrderDetail.as_view(), name='read'),
+    path('delete/<int:pk>/', OrderDelete.as_view(), name='delete'),
+    path('forming_complete/<int:pk>/', order_forming_complete, name='forming_complete'),
 
 ]
 
